c5bdfef1b53b7083075c97e49be39461d0571bd4
surasb11 <55266021+surasb11@users.noreply.github.com>
2019-10-22 14:10:50 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -12,88 +12,63 @@ with open(election_data_path, newline='') as election_data:
 	election_data_header = next(csvreader)
 	print(f"CSV Header: {election_data_header}")
 	
-	
-inputFileName = "election_data.csv"
-outputFileName = "election_data_script.txt"
+	inputFileName = "election_data.csv"
+	outputFileName = "election_data_script.txt"
 
-total_votes = 0
-winner = 0
-candidates = 0
-votes_percentage = 0
+	total_votes = 0
+	winner = 0
+	winner_candidate = ""
+	candidates = 0
+	votes_percentage = 0
+	winner_candidate_summary = 0
 
-greatest_votes = ["", 0]
-candidates_list = []
-candidate_votes = {}
+	greatest_votes = ["", 0]
+	candidates_list = []
+	candidate_votes = {}
 
 
-with open(election_data_path) as election_data:
-	reader = csv.DictReader(election_data)
-	
-	
-	for row in reader:
-		total_votes = total_votes + 1
-		total_candidates = row["Candidate"]
+	with open(election_data_path) as election_data:
+		reader = csv.reader(election_data)
+		header = next (reader)
 		
-		if row["Candidate"] not in candidates_list:
+		for row in reader:
+			total_votes = total_votes + 1
+			candidates_name = row[2]
 			
-			candidates_list.append(row["Candidate"])
+			if candidates_name not in candidates_list:
+				
+				candidates_list.append(candidates_name)
 
-			candidate_votes[row["Candidate"]] = 1
+				candidate_votes[candidates_name] = 0
+				
 			
-		else:
-			candidate_votes[row["Candidate"]] = candidate_votes[row["Candidate"]] + 1
+			candidate_votes[candidates_name] = candidate_votes[candidates_name] + 1
+	 
+	print(candidate_votes)
 
-	print("Election Results")
-	print("Total Votes " + str(total_votes))   
+	with open (outputFileName, "w") as txt_file:
+		election_results = (
+		f"Election Results\n"
+		f"-------------------------\n"
+		f"Total Votes {total_votes}\n"
+		f"-------------------------\n")
+		print(election_results, end="")
+		txt_file.write(election_results)
 
-	
-import pandas as pd
-import numpy as np
-
-inputFileName_df = pd.read_csv(inputFileName)
-inputFileName_df.head(10)
-
-total_votes = inputFileName_df["Voter ID"].count()
-inputFileName_df["Voter ID"].count()
-
-total_candidates = inputFileName_df['Candidate'].value_counts()
-
-unique_candidate = inputFileName_df["Candidate"].unique()
-unique_candidate
-
-from collections import OrderedDict
-from operator import itemgetter
-
-print()
-print()
-print()
-print("Election Results")
-print("-------------------------")
-print("Total Votes " + str(total_votes))
-print("-------------------------")
-
-
-for candidate in candidate_votes:
-	print(candidate + " " + str(round(((candidate_votes[candidate]/total_votes)*100))) + "%" + " (" + str(candidate_votes[candidate]) + ")") 
-	candidate_results = (candidate + " " + str(round(((candidate_votes[candidate]/total_votes)*100))) + "%" + " (" + str(candidate_votes[candidate]) + ")") 
-	
-	
-	candidate_votes
-	winner = sorted(candidate_votes.items(), key=itemgetter(1), reverse=True)
-	print("-------------------------")
-	print("Winner: " + str(winner[0]))
-	print("-------------------------")
-
-with open(outputFileName, "w") as txt_file:
-	txt_file.write("Election Results")
-	txt_file.write("\n")
-	txt_file.write("-------------------------")
-	txt_file.write("\n")
-	txt_file.write(candidate + " " + str(round(((candidate_votes[candidate]/total_votes)*100))) + "%" + " (" + str(candidate_votes[candidate]) + ")")
-	txt_file.write(str(winner))
-	txt_file.write("\n")
-	txt_file.write("-------------------------")
-	txt_file.write("\n")
-	txt_file.write("Winner: " + str(winner[0]))
-	txt_file.write("\n")
-	txt_file.write("Total Votes " + str(total_votes))
+		for candidate in candidate_votes:
+			votes = candidate_votes.get(candidate)
+			
+			vote_precentage = float(votes)/float(total_votes)*100
+			if (votes > winner):
+				winner = votes
+				winner_candidate = candidate
+			voter_output = f"{candidate}: {vote_precentage:.3f}% ({votes})\n"
+			print(voter_output, end="")
+			txt_file.write(voter_output)
+			
+		winning_candidate_summary = (
+		f"-------------------------\n"
+		f"Winner: {winner_candidate}\n"
+		f"-------------------------\n")
+		print(winning_candidate_summary)
+		txt_file.write(winning_candidate_summary)
